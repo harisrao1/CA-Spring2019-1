2ce608ca2945545cfbc5ac69e99712639797862e
harisrocker1 <harisrocker1@gmail.com>
2019-03-01 18:29:49 -0500

    Camera Follow player done B2
    faka u

---
+++ Assets/B2 stuff/Scripts/CamCollision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCollision : MonoBehaviour
+{
+    public float minDistance = 1;
+    public float maxDistance = 4;
+    public float smooth = 10;
+    Vector3 MannyDirection;
+    public Vector3 MannyDirectionAdjust;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MannyDirection = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3  desiredCamPos = transform.parent.TransformPoint(MannyDirection * maxDistance);
+        RaycastHit hit;
+
+        if(Physics.Linecast(transform.parent.position,desiredCamPos,out hit))
+        {
+            distance = Mathf.Clamp ((hit.distance*0.75f), minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, MannyDirection * distance, Time.deltaTime * smooth);
+    }
+}
--- Assets/B2 stuff/Scripts/CamFollow.cs
+++ Assets/B2 stuff/Scripts/CamFollow.cs
@@ -8,14 +8,13 @@ public class CamFollow : MonoBehaviour
     public float mouseSensitivity = 2;
     public GameObject CamObj;
     public GameObject PlayerObj;
-    public float clampAngle = 90;
+    public GameObject CamFollowObj;
+    public float clampAngle = 80;
     public float XDistance;
     public float YDistance;
     public float ZDistance;
     public float mouseX;
     public float mouseY;
-    public float finalX;
-    public float finalY;
     public float smoothX;
     public float smoothY;
     public float rotationX;
@@ -38,6 +37,27 @@ public class CamFollow : MonoBehaviour
     {
         mouseX = mouseSensitivity * Input.GetAxis("Mouse X");
         mouseY = mouseSensitivity * Input.GetAxis("Mouse Y");
-        PlayerObj.transform.Rotate(0, mouseX, 0);
+       //PlayerObj.transform.Rotate(0, mouseX, 0);
+
+        rotationY += mouseY  * Time.deltaTime;
+        rotationX += mouseX  * Time.deltaTime;
+        //PlayerObj.transform.Rotate(0, rotationX, 0);
+        rotationX = Mathf.Clamp(rotationX, -clampAngle, clampAngle);
+        rotationY = Mathf.Clamp(rotationY, -clampAngle, clampAngle);
+        Quaternion localRotation = Quaternion.Euler(rotationY, rotationX, 0);
+        transform.rotation = localRotation;
     }
+
+    void LateUpdate()
+    {
+        CameraFollower();
+    }
+
+    void CameraFollower()
+    {
+        Transform target = CamFollowObj.transform;
+        float step = CamMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
 }
