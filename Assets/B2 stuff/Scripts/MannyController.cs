e55ddd2d598b705ca82a34b4b948a3c73dcb99d5
harisrocker1 <harisrocker1@gmail.com>
2019-03-02 18:34:20 -0500

    ff

--- Assets/B2 stuff/Scripts/MannyController.cs
+++ Assets/B2 stuff/Scripts/MannyController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class MannyController : MonoBehaviour
 {
     public float speed = 4;
-    public float runspeed = 8;
+    public float inspeed = 4;
+    public float runspeed = 0.5f;
     public float rotationSpeed = 80;
-    public float gravity = 8;
+    public float gravity = 100;
+   // public float jumpspeed = 7;
     float rotation;
     float rotationX;
     float rotationY;
     Vector3 movedir = Vector3.zero;
+    private Rigidbody rb;
     
 
 
@@ -22,6 +25,7 @@ public class MannyController : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -33,35 +37,51 @@ public class MannyController : MonoBehaviour
 
         if (controller.isGrounded)
         {
-
+           
+            movedir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movedir = transform.TransformDirection(movedir);
+            movedir *= speed;
+            if (Input.GetKeyUp(KeyCode.LeftShift)) 
+            {
+                anim.SetInteger("run", 0);
+               // speed = runspeed;
+            }
             if (Input.GetKey(KeyCode.W))  // Move forward
             {
 
                 if (Input.GetKey(KeyCode.LeftShift))  // Move forward RUN
                 {
                     anim.SetInteger("run", 1);
-                    transform.position += transform.forward * Time.deltaTime * runspeed;
+                    speed = runspeed;
                 }
                 else
                 {
                     anim.SetInteger("run", 0);
-
                     anim.SetInteger("walk", 1);
-                    transform.position += transform.forward * Time.deltaTime * speed;
                     if (Input.GetKeyUp(KeyCode.S))
                     {
                         anim.SetInteger("jump", 0);
                         transform.position += transform.forward * Time.deltaTime * 0;
-
-
                     }
+                    speed = inspeed;
+
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    anim.SetInteger("walk", 0);
+                    anim.SetInteger("jump", 1);
+                    movedir.y = 4;
+                }
+                else
+                {
+                    anim.SetInteger("jump", 0);
+                    anim.SetInteger("walk", 1);
 
                 }
             }
             if (Input.GetKeyUp(KeyCode.W))
             {
                 anim.SetInteger("walk", 0);
-                transform.position += transform.forward * Time.deltaTime * 0;
                 
 
             }
@@ -70,46 +90,47 @@ public class MannyController : MonoBehaviour
 
                     if (Input.GetKey(KeyCode.LeftShift))  // Move backward RUN
                     {
-                        anim.SetInteger("run", 1);
-                        transform.position -= transform.forward * Time.deltaTime * runspeed;
+                        anim.SetInteger("run", 1);               
                     }
-
                     else
                     {
                         anim.SetInteger("run", 0);
-
                         anim.SetInteger("walk", 1);
-                        transform.position -= transform.forward * Time.deltaTime * speed;
-
                     }
             }
             if (Input.GetKeyUp(KeyCode.S))
             {
                 anim.SetInteger("walk", 0);
-                transform.position += transform.forward * Time.deltaTime * 0;
-                
-
             }
-            if (Input.GetKey(KeyCode.Space))
+            
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                anim.SetInteger("jump", 1);
-                
-                transform.position += transform.up * Time.deltaTime * speed;
-
-
+                if (anim.GetInteger("run") == 1)
+                {
+                    anim.SetInteger("runjump", 1);
+                    
+                    movedir.y = 4.5f;
+                }
+                else
+                {
+                    anim.SetInteger("jump", 1);
+                    movedir.y = 4;
+                }
             }
             else
             {
                 anim.SetInteger("jump", 0);
-
+                anim.SetInteger("runjump", 0);
             }
+
             
+
 
         }
 
-        
 
-        
+
+
         movedir.y -= gravity * Time.deltaTime;
         controller.Move(movedir * Time.deltaTime);
     }
