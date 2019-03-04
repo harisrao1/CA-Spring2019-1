55895691fc93e3a816cb90a31333eae936a64180
harisrocker1 <harisrocker1@gmail.com>
2019-03-04 15:34:27 -0500

    COLOR CHANG EON CLICK but no change back

--- Assets/AgentMoveAnimated.cs
+++ Assets/AgentMoveAnimated.cs
@@ -17,7 +17,10 @@ public class AgentMoveAnimated : MonoBehaviour
     private float z;
     public float jumpSpeed = 40;
     private bool onoffmesh;
-   // public float stopingDistance;
+    private Material[] skinMaterial;
+    private Color[] orignalColor;
+    
+    // public float stopingDistance;
     Vector3 destination = new Vector3(0, 0, 0);
     Ray ray;
     RaycastHit hit;
@@ -27,65 +30,73 @@ public class AgentMoveAnimated : MonoBehaviour
         director = getDirector.GetComponent<Director>();
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        
         //currentFloor = "anything";
         //safeFloor = "floor2";
     }
 
     void Update()
     {
+        
         if (agent.isOnOffMeshLink)
         {
             onoffmesh = true;
-        }
-        if (onoffmesh) {
             animator.SetInteger("jump", 1);
-            if ((agent.transform.position.x < agent.currentOffMeshLinkData.endPos.x) )
-            {
-                agent.transform.Translate(Vector3.back * Time.deltaTime * jumpSpeed);
-                
-            }
-            if ((agent.transform.position.x > agent.currentOffMeshLinkData.endPos.x))
-            {
-                
-                agent.transform.Translate(Vector3.forward * Time.deltaTime * jumpSpeed);
-               
-            }
-            if ((agent.transform.position.z < agent.currentOffMeshLinkData.endPos.z))
-            {
-                
-                agent.transform.Translate(Vector3.right * Time.deltaTime * jumpSpeed);
-                
-            } 
-            if ((agent.transform.position.z > agent.currentOffMeshLinkData.endPos.z))
-            {
-                
-                agent.transform.Translate(Vector3.left * Time.deltaTime * jumpSpeed);
-               
-            }
-
-            if (
-                   agent.transform.position.x >= agent.currentOffMeshLinkData.endPos.x - 0.2
-                && agent.transform.position.x <= agent.currentOffMeshLinkData.endPos.x + 0.2
-                && agent.transform.position.z >= agent.currentOffMeshLinkData.endPos.z - 0.2
-                && agent.transform.position.z <= agent.currentOffMeshLinkData.endPos.z + 0.2
-                )
-            {
-                Debug.Log("heyff");
-                agent.CompleteOffMeshLink();
-                onoffmesh = false;
-                agent.Resume();
-                
-
-            }
-            
         }
         else
         {
             animator.SetInteger("jump", 0);
         }
+        /* if (onoffmesh) {
+             animator.SetInteger("jump", 1);
+             if ((agent.transform.position.x < agent.currentOffMeshLinkData.endPos.x) )
+             {
+                 agent.transform.Translate(Vector3.back * Time.deltaTime * jumpSpeed);
+
+             }
+             if ((agent.transform.position.x > agent.currentOffMeshLinkData.endPos.x))
+             {
+
+                 agent.transform.Translate(Vector3.forward * Time.deltaTime * jumpSpeed);
+
+             }
+             if ((agent.transform.position.z < agent.currentOffMeshLinkData.endPos.z))
+             {
+
+                 agent.transform.Translate(Vector3.right * Time.deltaTime * jumpSpeed);
+
+             } 
+             if ((agent.transform.position.z > agent.currentOffMeshLinkData.endPos.z))
+             {
+
+                 agent.transform.Translate(Vector3.left * Time.deltaTime * jumpSpeed);
+
+             }
+
+             if (
+                    agent.transform.position.x >= agent.currentOffMeshLinkData.endPos.x - 0.2
+                 && agent.transform.position.x <= agent.currentOffMeshLinkData.endPos.x + 0.2
+                 && agent.transform.position.z >= agent.currentOffMeshLinkData.endPos.z - 0.2
+                 && agent.transform.position.z <= agent.currentOffMeshLinkData.endPos.z + 0.2
+                 )
+             {
+                 Debug.Log("heyff");
+                 agent.CompleteOffMeshLink();
+                 onoffmesh = false;
+                 agent.Resume();
+
+
+             }
+
+         }
+         else
+         {
+             animator.SetInteger("jump", 0);
+         }
+         */
         Debug.Log(onoffmesh);
         
-        if (agent.remainingDistance > 1.2f)
+        if (agent.remainingDistance > agent.stoppingDistance)
         {
             animator.SetInteger("walk", 1);
            // Debug.Log("Velocity" + agent.velocity);
@@ -93,6 +104,7 @@ public class AgentMoveAnimated : MonoBehaviour
         else
         {
             animator.SetInteger("walk", 0);
+            
         }
        
         if (Input.GetMouseButtonDown(0))
@@ -111,6 +123,9 @@ public class AgentMoveAnimated : MonoBehaviour
                 }
             }
         }
+        SkinnedMeshRenderer[] renderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+        skinMaterial = new Material[renderers.Length];
+        orignalColor = new Color[renderers.Length];
         if (director.selected == agent.tag)
         {
             if (agent.transform.parent.gameObject.tag == "nazguls")
@@ -119,9 +134,27 @@ public class AgentMoveAnimated : MonoBehaviour
             }
             else
             {
-                GetComponent<Renderer>().material.color = Color.yellow;
+                // GetComponent<Renderer>().material.color = Color.yellow;
+                
+                for (int i = 0; i < renderers.Length; i++)
+                {
+                    skinMaterial[i] = renderers[i].material;
+                   
+                    orignalColor[i] = skinMaterial[i].color;
+                }
+                
+                    skinMaterial[2].color = Color.red;
+                    skinMaterial[1].color = Color.red;
+
             }
         }
+        else
+        {
+            skinMaterial[2].color = Color.blue;
+            skinMaterial[1].color = Color.blue;
+
+        }
+       
     }
 
     
