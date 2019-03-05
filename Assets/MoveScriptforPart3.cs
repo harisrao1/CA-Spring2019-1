d4414e75a58cbe4685d46854d8f540b0e82e506c
harisrocker1 <harisrocker1@gmail.com>
2019-03-05 01:50:28 -0500

    Part 3 Kinda done

---
+++ Assets/MoveScriptforPart3.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class MoveScriptforPart3 : MonoBehaviour
+{
+    public Transform dest;
+    NavMeshAgent agent;
+    private Animator animator;
+    public float walkspeed;
+    private float dynamicwalkspeed;
+    private bool isStopped;
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(dest.position);
+        animator = this.GetComponent<Animator>();
+        dynamicwalkspeed = walkspeed;
+        isStopped = false;
+    }
+
+    
+    void Update()
+    {
+        if (isStopped)
+        {
+            animator.SetInteger("walk", 0);
+            agent.speed = 0;
+            agent.angularSpeed = 0;
+        }
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            
+            agent.speed = dynamicwalkspeed;
+            if (dynamicwalkspeed > 0 )
+            {
+                animator.SetInteger("walk", 1);
+                
+            }
+            else
+            {
+                dynamicwalkspeed = 0;
+                animator.SetInteger("walk", 0);
+            }
+        }
+        else
+        {
+            animator.SetInteger("walk", 0);
+        
+        }
+
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "agent1" && (other.isTrigger==false))
+        {
+            dynamicwalkspeed = 0;
+            Debug.Log("touching");
+        }
+        if (other.GetComponent<NavMeshAgent>().velocity == Vector3.zero)
+        {
+            isStopped = true;
+        }
+        
+     
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "agent1")
+        {
+            dynamicwalkspeed = walkspeed;
+           
+        }
+    }
+}
